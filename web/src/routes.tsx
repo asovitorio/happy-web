b9f189467c76090bbb3d3580ce4abd80d8a962ca
Alessandro <asovitorio@gmail.com>
2020-10-17 03:21:25 -0300

    ajustes nas telas

---
+++ web/src/routes.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter,
+} from "react-router-dom";
+
+export default ()=>{
+    return(
+        <BrowserRouter>
+            <Route>
+                
+            </Route>
+        </BrowserRouter>
+    )
+}
