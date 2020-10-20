aef5863d7696e18122fc4ba68c75564c94bd61f8
Alessandro <asovitorio@gmail.com>
2020-10-20 16:09:53 -0300

    teste ajustes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import './database/connection';
 import routes from './routes'
 import path from 'path'
+import cors from 'cors'
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use('/uploads',express.static(path.join(__dirname,'..','uploads')))
